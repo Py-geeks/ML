39820c687a016764db794458c2f6b411e4f30bf1
SAIRUS MISHRA <52784770+iamsairus10@users.noreply.github.com>
2021-10-10 12:51:24 +0530

    fresh update

---
+++ New.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#define ll long long int
+using namespace std;
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout); 
+#endif 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll a,b,c,p,q,r;
+		cin>>a>>b>>c;
+		cin>>p>>q>>r;
+		if((a==p)	&&	(b==q)	&&	(c==r))
+		{
+			cout<<"0"<<endl;
+		}
+		else if(((a!=p)	&&))
+
+	}
+
+
+
+
+
+
+
+}
+
